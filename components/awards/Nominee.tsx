38e95bd9cb9896995eee2e7bef8fee5d9159f72b
adonis0907 <adonis0907@outlook.com>
2022-03-09 06:21:57 +0900

    feat: requirement 4-define layout and hover styles

--- components/awards/Nominee.tsx
+++ components/awards/Nominee.tsx
@@ -1,20 +1,20 @@
 import type { NextComponentType } from 'next'
-
+import styles from "@styles/awards/Nominee.module.css";
 
 const Nominee: NextComponentType = () => {
     return (
-        <div>
+        <div className={ styles.nominee }>
             <div className="row">
                 <h3 className="text-center">Nominee</h3>
             </div>
             <div className="row">
                 <div className="col justify-content-center">
-                    <img src="https://variety.com/wp-content/uploads/2020/12/nomadland_ver2.jpg" />
+                    <img className={ styles['nominee-img'] } src="https://variety.com/wp-content/uploads/2020/12/nomadland_ver2.jpg" />
                 </div>
             </div>
             <div className="row">
                 <div className="col justify-content-center">
-                    <button>Select</button>
+                    <button className={ styles['nominee-select'] }>Select</button>
                 </div>
             </div>
         </div>
